fcecec4b6ef8742da368476c8988b6d421f53810
Anirudh Sivaraman <sk.anirudh@gmail.com>
2019-11-01 22:29:40 -0700

    Cleanup code and add some comments.

--- sim.py
+++ sim.py
@@ -1,4 +1,3 @@
-#! /opt/local/bin/python3
 from numpy.random import poisson
 import sys
 
@@ -7,24 +6,27 @@ if (len(sys.argv) < 4):
   sys.exit(1)
 else:
   load             = float(sys.argv[1])
+  assert(load < 1)
   num_sources      = int(sys.argv[2])
   buffer_size      = int(sys.argv[3])
   ticks            = 100000
-  input_rates      = [load] * num_sources
+  input_rates      = [load] * num_sources # initialize to user-specified load
   queue_size       = 0
 
+# Simulate tick by tick
 for t in range(0, ticks):
-  # arrival for each source
+  # Arrival process for each source
   for j in range(0, num_sources):
-    packet_arrivals = poisson(input_rates[j])
+    packet_arrivals = poisson(input_rates[j]) # Poisson arrivals
     assert(queue_size <= buffer_size)
-    if queue_size + packet_arrivals <= buffer_size:
+    if queue_size + packet_arrivals <= buffer_size: # space in buffer
       queue_size += packet_arrivals
-      input_rates[j] = load # min(input_rates[j] * 2.0, load)
+      assert(queue_size <= buffer_size)
+      input_rates[j] = min(input_rates[j] * 2.0, load) # multiplicative increase
     else:
       queue_size = buffer_size
-      input_rates[j] = input_rates[j] / 2.0
+      input_rates[j] = input_rates[j] / 2.0 # multiplicative decrease
 
-  # departure
+  # Deterministic departure process
   queue_size = max(0, queue_size - num_sources)
 print("Input rate at the end is ", sum(input_rates)/num_sources)
